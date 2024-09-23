251e928f84ba6cfd49ad8d0c2307d88726654dec
Mauri Astudillo <m.astudillo@duocuc.cl>
2024-09-23 00:59:44 -0300

    Lista de conductores ejemplo

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import { addIcons } from 'ionicons';
+import { add } from 'ionicons/icons';
+
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -7,6 +10,8 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor() {
+    addIcons({ add });
+  }
 
 }
