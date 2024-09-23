7a04ac9674c235586fd2bb44bf9afa63b0022dfe
Mauri Astudillo <m.astudillo@duocuc.cl>
2024-09-23 00:44:25 -0300

    cambios y botones

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,10 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'reservas',
+    loadChildren: () => import('./reservas/reservas.module').then( m => m.ReservasPageModule)
   }
 ];
 @NgModule({
