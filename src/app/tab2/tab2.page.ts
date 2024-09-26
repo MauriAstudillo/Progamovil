07f019960a4240ea03456eede59ce7a1c511a965
Mauri Astudillo <m.astudillo@duocuc.cl>
2024-09-26 00:15:59 -0300

    pings

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -6,7 +7,51 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  constructor(private alertController: AlertController) {}
 
-  constructor() {}
+  async presentCancelAlert() {
+    const alert = await this.alertController.create({
+      header: 'Confirmar cancelación',
+      message: '¿Estás seguro de que deseas cancelar el viaje?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          cssClass: 'secondary',
+        },
+        {
+          text: 'Aceptar',
+          handler: () => {
+            // Aquí puedes manejar la acción de cancelar el viaje
+            console.log('Viaje cancelado');
+          },
+        },
+      ],
+    });
 
+    await alert.present();
+  }
+
+  async presentConfirmAlert() {
+    const alert = await this.alertController.create({
+      header: 'Confirmar viaje',
+      message: '¿Estás seguro de que deseas confirmar el viaje?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          cssClass: 'secondary',
+        },
+        {
+          text: 'Aceptar',
+          handler: () => {
+            // Aquí puedes manejar la acción de confirmar el viaje
+            console.log('Viaje confirmado');
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+  }
 }
